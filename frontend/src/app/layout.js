7b6e0ff8dcbe17e0cfe26e673fbf2216f2a2ee08
nandanamv3 <nandanaattore@gmail.com>
2024-10-22 08:00:59 +0530

    changed font to poppins

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,15 +1,10 @@
-import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// Define the Poppins font
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
 });
 
 export const metadata = {
@@ -21,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`} // Apply the Poppins font
       >
         {children}
       </body>
